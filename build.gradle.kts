53ab2717f315e79ab38e48a5c9ab58111f5ea66b
MKagua <mkagua@safaricom.co.ke>
2026-03-04 10:19:09 +0300

    release: 1.0.0-alpha02

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "io.github.kagua98"
-version = "1.0.0-alpha01"
+version = "1.0.0-alpha02"
 
 kotlin {
     androidTarget {
@@ -84,7 +84,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.kagua98",
         artifactId = "pulse",
-        version = "1.0.0-alpha01",
+        version = "1.0.0-alpha02",
     )
 
     pom {
